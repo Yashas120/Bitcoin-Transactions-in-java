3b52e3dc965fc5d89108108aaa19c45f995d0df5
Vishal R <vishalramesh01@gmail.com>
2022-02-15 18:23:49 +0530

    Add Test Cases and Setup JUnit
    Add test cases for hashing and ecc packages. Corrected few bugs.

--- src/test/java/ecc/eccTest.java
+++ src/test/java/ecc/eccTest.java
@@ -1,5 +1,109 @@
 package ecc;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+
+import org.junit.Test;
+
 public class eccTest {
+  BigInteger p = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16);
+  BigInteger a = new BigInteger("0000000000000000000000000000000000000000000000000000000000000000", 16);
+  BigInteger b = new BigInteger("0000000000000000000000000000000000000000000000000000000000000007", 16);
+  BigInteger x = new BigInteger("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16);
+  BigInteger y = new BigInteger("483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 16);
+  BigInteger n = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16);
   
+  public static String toHex(String arg) {
+    return String.format("%040x", new BigInteger(1, arg.getBytes(Charset.forName("UTF-8"))));
+  }
+
+  @Test
+  public void point_short_add_test(){
+    Curve curve = new Curve(p, a, b);
+    Point G = new Point(curve, x,y);
+    // String secret_key = "3";
+    G = G.add(G).add(G);
+    BigInteger expectedx = new BigInteger("112711660439710606056748659173929673102114977341539408544630613555209775888121");
+    BigInteger expectedy = new BigInteger("25583027980570883691656905877401976406448868254816295069919888960541586679410");
+    String description = "Short ECC point add";
+    try{
+      assertEquals(expectedx, G.x);
+      assertEquals(expectedy, G.y);
+      assertTrue(G.verify_on_curve());
+      System.out.println(description + " - \033[92mpassed\033[0m");
+    }catch(AssertionError e){
+        System.out.println(description + " - \033[91mfailed\033[0m");
+      throw e;
+    }
+  }
+
+  @Test
+  public void point_long_add_test(){
+    Curve curve = new Curve(p, a, b);
+    Point G = new Point(curve, x,y);
+    // String secret_key = "3";
+    G = G.add(G).add(G).add(G).add(G).add(G).add(G).add(G).add(G).add(G);
+    BigInteger expectedx = new BigInteger("72488970228380509287422715226575535698893157273063074627791787432852706183111");
+    BigInteger expectedy = new BigInteger("62070622898698443831883535403436258712770888294397026493185421712108624767191");
+    String description = "Long ECC point add";
+    try{
+      assertEquals(expectedx, G.x);
+      assertEquals(expectedy, G.y);
+      assertTrue(G.verify_on_curve());
+      System.out.println(description + " - \033[92mpassed\033[0m");
+    }catch(AssertionError e){
+        System.out.println(description + " - \033[91mfailed\033[0m");
+      throw e;
+    }
+  }
+
+  @Test
+  public void point_space_secret_key(){
+    Curve curve = new Curve(p, a, b);
+    Point G = new Point(curve, x,y);
+    BigInteger secretKey = new BigInteger(toHex(" "),16);
+    G = G.multiply(secretKey);
+    BigInteger expectedx = new BigInteger("95440839670107969455973995843666399663662641812074432045896568980475242364517");
+    BigInteger expectedy = new BigInteger("67400892360194400039319989411395972789004161889863182881857158544061243615929");
+    String description = "space secret key";
+    try{
+      assertEquals(new BigInteger("32"), secretKey);
+      assertEquals(expectedx, G.x);
+      assertEquals(expectedy, G.y);
+      assertTrue(G.verify_on_curve());
+      System.out.println(description + " - \033[92mpassed\033[0m");
+    }catch(AssertionError e){
+        System.out.println(description + " - \033[91mfailed\033[0m");
+      throw e;
+    }
+  }
+
+  @Test
+  public void point_tough_secret_key(){
+    Curve curve = new Curve(p, a, b);
+    Point G = new Point(curve, x,y);
+    
+    String secretKey_string = "_yrk<Rz{xtNI2nxh-q8mvE8ukwbDEOQLH)rs&kWA)kXKo*r m>be7Ng*RHGfLgut08*Ew>IwwD{h?fE)RB(7nFLuPylyQiAxYhxwMbyd^(R&>oy6WBOkT&yWPuwHBt{BqH-pFqmA2PM)AzgVM_UM;6{$tJiT+6h {kPCmGkvPUh6B?)x_ng{x@*o(tSvA|Ak@tV(ob=";
+    BigInteger expectedSecretKey = new BigInteger("64773904892189424650623596182827208874631779838793845654828791137613274747315303641475554299193235089603468180846331886664752858470421018921535490911511470433104687394241339153172152119108174652136127198312106667208480599084513077338160285497797897998727812728967149866059980346212051898926039084711561827157754147130313308450509984756994272841997584202787717845249019157295985838714551027191937697729171991861666249716524343794439358993266349651490401996309244661926592578019901");
+
+    BigInteger secretKey = new BigInteger(toHex(secretKey_string),16);
+    
+    G = G.multiply(secretKey);
+    BigInteger expectedx = new BigInteger("93709308251719379171912892844854393740387970949337600343564883758518494492811");
+    BigInteger expectedy = new BigInteger("113255582048810676581111405841386767654579450998162618008269151592948100713260");
+    String description = "tough secret key";
+    try{
+      assertEquals(expectedSecretKey, secretKey);
+      assertEquals(expectedx, G.x);
+      assertEquals(expectedy, G.y);
+      assertTrue(G.verify_on_curve());
+      System.out.println(description + " - \033[92mpassed\033[0m");
+    }catch(AssertionError e){
+        System.out.println(description + " - \033[91mfailed\033[0m");
+      throw e;
+    }
+  }
 }
---
+++ src/test/java/hashing/HashTest.java
@@ -0,0 +1,105 @@
+package hashing;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HashTest{
+
+    @Test
+    public void test_empty_sha256(){
+        Sha hash = new Sha();
+        String actual = hash.sha256("");
+        String expected = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+        String description = "Empty string hash test";
+        try{
+            assertEquals(expected, actual);
+            System.out.println(description + " - \033[92mpassed\033[0m");
+        }catch(AssertionError e){
+            System.out.println(description + " - \033[91mfailed\033[0m");
+          throw e;
+        }
+    }
+
+    @Test
+    public void test_short_sha256(){
+        Sha hash = new Sha();
+        String actual = hash.sha256("OOAD-short-test");
+        String expected = "ed17860dbe4ec22b048fe047bb61972c2e0411da97c271967113b5231191f8ad";
+        String description = "Short string hash test";
+        try{
+            assertEquals(expected, actual);
+            System.out.println(description + " - \033[92mpassed\033[0m");
+        }catch(AssertionError e){
+            System.out.println(description + " - \033[91mfailed\033[0m");
+          throw e;
+        }
+    }
+
+    @Test
+    public void test_medium_sha256(){
+        Sha hash = new Sha();
+        String actual = hash.sha256("An aim is a goal or objective to achieve in life. " + 
+                                    "In order to succeed in life, one must have a goal. " + 
+                                    "My aim in life is to be a teacher. " + 
+                                    "Teaching is a noble and responsible profession. " + 
+                                    "I have come to know that the ever-increasing misery and distress, are due to the ignorance and illiteracy of the people of our country. " + 
+                                    "So I have decided to spread education among the masses as much as possible within my humble power. " + 
+                                    "As a teacher, I shall try my best to impart man-making education. Some say that money is the honey of life. " + 
+                                    "But I do not agree with them. Rather, I think that morality is the real honey of life. " + 
+                                    "I want to be a lovable and respectable person as a teacher in the future."
+                                );
+        
+        String expected = "b0b39c997dddf9350ad31d3f474748aa4206726992a5ef3f6de7c55e2b79a243";
+        String description = "Medium string hash test";
+        try{
+            assertEquals(expected, actual);
+            System.out.println(description + " - \033[92mpassed\033[0m");
+        }catch(AssertionError e){
+            System.out.println(description + " - \033[91mfailed\033[0m");
+          throw e;
+        }
+    }
+
+    @Test
+    public void test_hard_sha256(){
+        Sha hash = new Sha();
+        String actual = hash.sha256("An00aim||||life75756htg977v899 85757is 8578a 986988goal or objective to ach||||life75756htgieve in life. " + 
+                                    "In ordeching is a noble and respon||||life75756htgsibl;;;wl;**37021:00031+++++++++++__+_+_+_r to succeed idawdawd{}n life, one must have a goal. " + 
+                                    "My aim in ;;;;||||||life757||||life75756htg56htghfsdad is to be a8678678 teacher. " + 
+                                    "Teach||||lif||||life75756htge75756htging is a noble and||||life75756htg responsibl;;;wl;**37021:00031+++++++++++__+_+_+_+_e profession. " + 
+                                    "I havve come to know that t||||life75756htghe ever-increasing misery and distress, are due to the ignorance and illiteracy of the people of our country. " + 
+                                    "So I hav||||life75756htge decided to spreaching ;;;;;;;;;;;;;;;;is a noble ||||life75756htgand responsibl;;;wl;**37021:00031+++++++++++__+_+_+_d education among the masses as much as possible within my humble power. " + 
+                                    "As a teacher||||life7||||life75756htg5756htg, I shall try my best to impart man-making ching is a noble and responsibl;;;wl;**37021:00031+++++++++++__+_+_+_education. Some say that money is the honey of life. " + 
+                                    "But I doching is a noble and responsibl;;;wl;**37021:00031+++++++++++__+_+_+_ not agree with them. Rather, I think that morality is the real honey of life. " + 
+                                    "I want to be||||life75756htg a lov||||life75756htgable and re||||life75756htgspectable person as a teacher in the future."
+                                );
+        
+        String expected = "bf38611267d7c973c23dd7eac3cc86c820d6e58e22ee66e36e554cd9a22ff0ae";
+        String description = "hard string hash test";
+        try{
+            assertEquals(expected, actual);
+            System.out.println(description + " - \033[92mpassed\033[0m");
+        }catch(AssertionError e){
+            System.out.println(description + " - \033[91mfailed\033[0m");
+          throw e;
+        }
+    }
+
+
+    // public void test_ripemd160(){
+    //     String str="a";
+    //     for(int i=0;i<1000;i++){
+    //         str += str;
+    //     }
+    //     String[] test = {"","a","abc","message digest","12345678901234567890123456789012345678901234567890123456789012345678901234567890",str};
+    //     String[] res = {"9c1185a5c5e9fc54612808977ee8f548b2258d31","0bdc9d2d256b3ee9daae347be6f4dc835a467ffe","8eb208f7e05d987a9b044a8e98c6b087f15a0bfc","5d0689ef49d2fae572b881b123a85ffa21595f36","9b752e45573d4b39f4dbd3323cab82bf63326bfb","aa69deee9a8922e92f8105e007f76110f381e9cf"};
+
+    //     // Ripemd ripemd160 = new Ripemd();
+
+    //     for(int b=0; b<test.length;b++){
+    //         // yolo = String.format("%02x", 0xff & ripemd160(test[b].getBytes(StandardCharsets.US_ASCII)));
+    //         // assert yolo == res[b];
+    //     }
+    // }
+}
+
