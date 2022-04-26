5669232810aed905d5a5c53e81898a833c24b92c
yashas120 <yashas120@gmail.com>
2022-04-26 21:04:37 +0530

    Update Wallet

--- src/main/java/dashboard/wallet.java
+++ src/main/java/dashboard/wallet.java
@@ -4,12 +4,18 @@ import java.math.BigInteger;
 import java.nio.charset.Charset;
 import org.json.JSONObject;
 import org.json.JSONException;
+import org.json.JSONArray;
 import ecc.Curve;
 import ecc.Generator;
 import ecc.Point;
 import ecc.PublicKey;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-public class Wallet{
+public class Wallet {
 
     String name;
     String secretKey;
@@ -24,12 +30,10 @@ public class Wallet{
     Curve bitcoinCurve = new Curve(p,a,b);
     Point G = new Point(bitcoinCurve,x,y);
     Generator bitcoinGenerator = new Generator(G, n);
-    
     public static String toHex(String arg) {
         return String.format("%040x", new BigInteger(1, arg.getBytes(Charset.forName("UTF-8"))));
-    }
-
-    public Wallet( String name, String key){
+      }
+    Wallet( String name, String key){
         this.name = name;
         this.secretKey = key;
         this.balance = 0;
@@ -39,26 +43,41 @@ public class Wallet{
         System.out.println("Secret Key : "+secretKey);
         System.out.println("Public Key : \nx : "+publicKey.x+"\ny : "+publicKey.y);
         System.out.println("Public Key generated is on curve : \033[92m" + publicKey.verify_on_curve()+"\033[0m");
-        String pbk1 = PublicKey.toPublicKey(publicKey).address("test", true);
-        System.out.println("\nBitcoin addr : "+pbk1);
-        System.out.println("Length of addr : "+pbk1.length());
+        this.addr = PublicKey.toPublicKey(publicKey).address("test", true);
+        System.out.println("\nBitcoin addr : "+ this.addr);
+        System.out.println("Length of addr : "+this.addr.length());
         System.out.println("-------------------------------------------------------------------");
     }
 
-    public Boolean newWallet(){
-    try {
-            JSONObject jsonObject = new JSONObject("{\"phonetype\":\"N95\",\"cat\":\"WP\"}");
-            System.out.println(jsonObject);
-    }
-    catch (JSONException err){
-            System.out.println("Error : " + err.toString());
-    }
+    public Boolean newWallet() throws IOException, InterruptedException, JSONException{
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://blockstream.info/testnet/api/address/" + this.addr))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        // System.out.println((response));
+        JSONObject body = new JSONObject(response.body());
+        // System.out.println(body);
+        JSONObject chain_stats = (JSONObject)(body.get("chain_stats"));
+        // System.out.println(body.get("chain_stats"));
+        // System.out.println(chain_stats.get("tx_count"));
+        if((int)chain_stats.get("tx_count") == 0){
+            return true;
+        }
+    
 
         return false;
     }
 
     public static void main(String[] args) {
-        Wallet w = new Wallet("test wallet", "vishal");
-        System.out.println(w.newWallet());
+        try{
+
+            Wallet w = new Wallet("test wallet", "vishal");
+            System.out.println("Is new Wallet ? " + w.newWallet());
+        }
+        catch(Exception err){
+            System.out.println(err);
+        }
     }
 }
